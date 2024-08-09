0a0469ab4a57679bfef5c97f357e9fdc22c3a61c
Wayne Mao <helloint@qq.com>
2024-08-09 12:19:09 +0800

    feat: support multiple sources

--- scripts/syncIptv.js
+++ scripts/syncIptv.js
@@ -5,29 +5,75 @@ import fs from 'fs';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const dataFilePath = `${__dirname}/../iptv/`;
 
-const allowedGenreNames = {
-    '央视频道': '央视',
-    '卫视频道': '卫视',
-    '上海频道': '上海',
-};
-const genreSeparator = '\n\n';
-const genreNameSeparator = ',#';
+const GENRE_SEPARATOR = '\n\n';
+const GENRE_NAME_SEPARATOR = ',#';
+const GENRE_NAME_REGEX = /group-title="([^"]*)"/;
+
+const SOURCES = [
+    {
+        id: 'fmml_ipv6_sh',
+        alias: 'fmml',
+        url: 'https://m3u.ibert.me/txt/fmml_ipv6.txt',
+        genres: {
+            '央视频道': '央视',
+            '卫视频道': '卫视',
+            '上海频道': '上海',
+        }
+    },
+    {
+        id: 'aptv',
+        url: 'https://raw.githubusercontent.com/Kimentanm/aptv/master/m3u/iptv.m3u'
+    }
+];
 
 const main = async () => {
-    const url = 'https://m3u.ibert.me/txt/fmml_ipv6.txt';
-    const genres = await fetchIptv(url);
-    const filteredGenres = [];
-    for (const genre of genres.split(genreSeparator)) {
-        const genreName = genre.split(genreNameSeparator)[0];
-        if (allowedGenreNames[genreName]) {
-            filteredGenres.push([allowedGenreNames[genreName], genre.split(genreNameSeparator)[1]].join(genreNameSeparator));
+    for (const source of SOURCES) {
+        const filteredSource = await filterGenre(source.url, source.genres);
+        fs.writeFileSync(`${dataFilePath}${source.id}.txt`, filteredSource);
+        fs.writeFileSync(`${dataFilePath}${source.id}.m3u`, filteredSource);
+        if (source.alias) {
+            fs.writeFileSync(`${dataFilePath}${source.alias}.txt`, filteredSource);
+            fs.writeFileSync(`${dataFilePath}${source.alias}.m3u`, filteredSource);
         }
     }
-
-    const newGenres = filteredGenres.join(genreSeparator);
-
-    fs.writeFileSync(`${dataFilePath}fmml_ipv6_sh.txt`, newGenres);
 };
+
+const filterGenre = async (sourceUrl, allowedGenres) => {
+    const source = await fetchIptv(sourceUrl);
+    if (allowedGenres) {
+        const filteredGenres = [];
+        for (const genre of source.split(GENRE_SEPARATOR)) {
+            const genreName = getGenreName(genre);
+            if (genreName && genreName in allowedGenres) {
+                const genreAlias = allowedGenres[genreName];
+                if (genreAlias) {
+                    // FIXME: Doesn't support type 2
+                    filteredGenres.push([allowedGenres[genreName], genre.split(GENRE_NAME_SEPARATOR)[1]].join(GENRE_NAME_SEPARATOR));
+                } else {
+                    filteredGenres.push(genre);
+                }
+            }
+        }
+
+        return filteredGenres.join(GENRE_SEPARATOR);
+    }
+
+    return source;
+}
+
+/**
+ * 2 Different types of genre name:
+ *   央视频道,#genre#
+ *   #EXTINF:-1 tvg-id="CHC家庭影院" tvg-name="CHC家庭影院" tvg-logo="https://live.fanmingming.com/tv/CHC家庭影院.png" group-title="央视IPV4",CHC家庭影院
+ * @param genre
+ * @returns {*}
+ */
+const getGenreName = (genre) => {
+    const result = genre.match(GENRE_NAME_REGEX);
+    if (result) return result[1];
+
+    return genre.split(GENRE_NAME_SEPARATOR)[0];
+}
 
 const fetchIptv = async (url) => {
     const options = {
